fad66513a52bf7f8e275f51e49bc6ffda30208a4
ish29-code <ishikadeshpande816@gmail.com>
2026-02-03 23:39:30 +0530

    reademe.md

--- backend/config/socketRedis.js
+++ backend/config/socketRedis.js
@@ -1,10 +1,26 @@
 // config/socketRedis.js
 
-import { createClient } from "redis";
+/*import { createClient } from "redis";
 
 
 //export const pubClient = createClient({ url: process.env.REDIS_URL });
 export const subClient = pubClient.duplicate();
 
+await pubClient.connect();
+await subClient.connect();
+*/
+
+import { createClient } from "redis";
+
+const redisUrl = process.env.REDIS_URL;
+
+/* ✅ EXPORT HERE */
+export const pubClient = createClient({
+    url: redisUrl,
+});
+
+export const subClient = pubClient.duplicate();
+
+/* connect */
 await pubClient.connect();
 await subClient.connect();
