bdd3fd2ae6a3753c0ec465bad873f2a1799f583a
ish29-code <ishikadeshpande816@gmail.com>
2026-02-03 22:46:33 +0530

    reademe.md

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 /* ===== GLOBAL MIDDLEWARE ===== */
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true,
   allowedHeaders: ["Authorization", "x-auth-type", "Content-Type"],
 }));
